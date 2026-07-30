1c1d46b0cd275d78473b698be5a7dd95c01c4e15
Colin-Fredericks <Colin-Fredericks@users.noreply.github.com>
2026-07-30 16:34:48 -0400

    Initial commit for file comparison

---
+++ file_compare/README.md
@@ -0,0 +1,42 @@
+# File Comparison Problem
+
+## Current Status
+
+First commit - most code is currently left over from copying the journaling and dropfile problem types.
+
+## Overview
+
+This problem compares submitted files to a particular set of files in the `/static/` folder, or to a set of sha256 hashes.
+
+### Grading
+
+- Turn partial credit on or off in the `options` variable. If partial credit is off, only 100% correctness receives credit.
+- If we're using hashes, the score returned is the percentage of files whose hashes match exactly.
+- If we're using files, each file can get partial credit. Perfect matches give full credit, but if things differ in the ways listed below, partial credit can be obtained. Partial credit values are multiplied together to get the final score.
+  - `spaces` is applied if whitespace at beginning and end of lines does not match.
+  - `crlf` is applied if there are extra blank lines.
+  - `case` is applied if things match only once case is squished.
+  - To make one of these *required*, set it to 0 so that it will zero out the score on its own. To *ignore* it, set it to 1 so that missing that item doesn't alter the score.
+  - Scores below the `low_cutoff` are rounded down to 0%.
+  - Scores above the `high_cutoff` are rounded up to 100%.
+
+## Creating Your Own File Comparison Problems
+
+1. Get the `python_lib.zip` file from [HX-PY](https://github.com/Colin-Fredericks/hx-py) and upload it to your Files page. *Keep it zipped.*
+2. Download `jschannel.js`, `file_comparison.html` and `file_comparison.js`, and upload them to your Files page.
+3. Download `file_comparison.xml`, edit the prompt as desired, and paste that into a Blank Advanced Problem.
+4. Adjust the python in your problem to include either the filenames for comparison or the hashes you want to compare to.
+
+## Notes
+
+- You can create multiple File Comparison problems on the same page.
+- The hashing algorithm used is **SHA-256**. If you call your algorithm on the string `test` and you get `9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08` back, you are probably using the right algorithm.
+
+## Files
+
+Inside you will find many pieces used to create this problem:
+
+- `jschannel.js`: the javascript file that allows javascript-type problems to work at all. You should not need to change this. I didn't make this one.
+- `file_comparison.js`: the javascript file that does the majority of the work. You should not need to change this.
+- `file_comparison.html`: the HTML file that will be iframed into the page. You should not need to change this.
+- `file_comparison.xml`: the XML used to create the problem within Studio. You will need to make changes to this, replacing the prompt with your own and listing filenames or hashes as mentioned above.
---
+++ file_compare/file_comparison.js
@@ -0,0 +1,144 @@
+// If the student comes to this problem for the first time,
+// they start with a blank.
+var JSProblemState = {
+  files: [],
+};
+
+// This wrapper function is necessary.
+// You can rename it if you want, just make sure the attributes
+// in your <jsinput> tag match the function name here.
+// This wrapper function is necessary.
+var file_comparison = (function() {
+
+	// REQUIRED --- DO NOT REMOVE/CHANGE!!
+	var channel;
+
+	// REQUIRED --- DO NOT REMOVE/CHANGE!!
+	if (window.parent !== window) {
+		channel = Channel.build({
+			window: window.parent,
+			origin: "*",
+			scope: "JSInput"
+		});
+		channel.bind("getGrade", getGrade);
+		channel.bind("getState", getState);
+		channel.bind("setState", setState);
+
+	}
+	
+	// getState() and setState() are required by the problem type.
+	function getState(){
+		console.log('getting state');
+		return JSON.stringify(JSProblemState);
+	}
+
+	function setState() {
+		console.log('setting state');
+		let stateStr = arguments.length === 1 ? arguments[0] : arguments[1];
+		JSProblemState = JSON.parse(stateStr);
+	}
+
+	
+	function getGrade() {
+		console.log('getting grade');
+		
+		// Log the problem state. 
+		// This is called from the parent window's Javascript so that we can write to the official edX logs. 
+		parent.logThatThing(JSProblemState);
+
+		// Return the whole problem state.
+		return JSON.stringify(JSProblemState);
+	}
+	
+	// REQUIRED --- DO NOT REMOVE/CHANGE!!
+	return {
+		getState: getState,
+		setState: setState,
+		getGrade: getGrade
+	};
+
+}());
+
+
+/**
+ * 
+ * @param {*} file 
+ * @param {*} outputArea 
+ * @param {*} infoArea 
+ */
+function processFile(file, outputArea, infoArea){
+
+	JSProblemState.uploadedRightThing = true;
+
+	// Give us the basics about the file.
+	infoArea.append('<p>Filename: ' + file.name + '</p>');
+	infoArea.append('<p>Type: ' + file.type + '</p>');
+	infoArea.append('<p>Size: ' + file.size + ' bytes</p>');
+	
+	if(file.type.indexOf('image') > -1){
+	
+		// Display a small preview of the image. Using Data URI approach to load.
+
+		var imageData = new FileReader();
+		imageData.onload = function(event) {
+			var dataUri = event.target.result,
+				img = document.createElement('img');
+
+			img.src = dataUri;
+			outputArea.append(img);
+		};
+
+		imageData.onerror = function(event) {
+			console.error('File could not be read! Code ' + event.target.error.code);
+			JSProblemState.uploadedRightThing = false;
+		};
+
+		imageData.readAsDataURL(file);
+		
+		JSProblemState
+	
+	}else if(file.type.indexOf('text') > -1){
+	
+		// Show the full content as preformatted text.
+	
+		var textData = new FileReader();
+		textData.onload = function(event) {
+			var rawText = event.target.result;
+			outputArea.append('<pre></pre>');
+			$('pre:last-child').text(rawText).html();
+		};
+
+		textData.onerror = function(event) {
+			console.error('File could not be read! Code ' + event.target.error.code);
+			JSProblemState.uploadedRightThing = false;
+		};
+
+		textData.readAsText(file);
+	
+	}else{
+		
+		// This is not what we asked for.
+		outputArea.append('<p>That is not an image or text file.');
+		JSProblemState.uploadedRightThing = false;
+	
+	}
+
+}
+
+
+/**
+ * Hashes text to SHA256 for the purpose of comparing answers without revealing the answer itself.
+ * Taken from https://stackoverflow.com/a/70243259/1330737
+ *
+ * @param {string} source
+ * @returns {Promise<string>}
+ */
+async function sha256(source) {
+  const sourceBytes = new TextEncoder().encode(source);
+  const digest = await crypto.subtle.digest("SHA-256", sourceBytes);
+  const resultBytes = [...new Uint8Array(digest)];
+  return resultBytes.map((x) => x.toString(16).padStart(2, "0")).join("");
+}
+
+// Just letting us know that the iframe is working.
+console.log("inner ready");
